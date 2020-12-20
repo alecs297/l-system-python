07d30fd3d31b8d2d6ff39a1f522c8ae4b53ee840
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2020-12-20 19:39:08 +0100

    finished my part

--- tortue.py
+++ tortue.py
@@ -4,19 +4,21 @@ from inspect import getsource
 from turtle import *
 
 
-default_jump = 3
+default_jump = 5
 default_angle = 45
 default_speed = 0
 default_input = "+a[a]++[a]*a"
 default_file = "output.py"
 
 points = []
-output = "leave.py"
-axiom = "----a"
-rules = ["a=aa+[+a-a-a]-[-a+a+a]"]
-angle = 22.5
-level = 4
+output = default_file
 
+axiom = "a-a-a-a"
+rules = [
+     "a=a+a*a+a+aa*aa+a+a*a+a"
+]
+angle = 90
+level = 3
 
 def custom_log(type, message):
     print("{} [{}] {}".format(strftime("%H:%M:%S", localtime()), type.upper(), message))
@@ -24,24 +26,24 @@ def custom_log(type, message):
 def a():
     pd()
     fd(default_jump)
-    return ("pd;fd({});".format(default_jump))
+    return ("pd;fd({})\n".format(default_jump))
 
 def b():
     pu()
     fd(default_jump)
-    return ("pu;fd({});".format(default_jump))
+    return ("pu;fd({})\n".format(default_jump))
 
 def plus():
     right(angle)
-    return ("right({});".format(angle))
+    return ("right({})\n".format(angle))
 
 def minus():
     left(angle)
-    return ("left({});".format(angle))
+    return ("left({})\n".format(angle))
 
 def snap():
     right(180)
-    return ("right(180);")
+    return ("right(180)\n")
 
 def save_point(points):
     points.append({"linear": pos(), "rotational": heading()})
@@ -50,7 +52,7 @@ def save_point(points):
 def jump_to(pos):
     goto(pos["linear"])
     setheading(pos["rotational"])
-    return("goto({});setheading({});".format(pos["linear"], pos["rotational"]))
+    return("goto({});setheading({})\n".format(pos["linear"], pos["rotational"]))
 
 def rewind_to_point(points):
     r = -1
@@ -77,7 +79,7 @@ def parse_levels(axiom, levels, rules):
     return axiom
 
 def treat_axiom(axiom, m, output):
-    r = "from turtle import *;speed({});".format(default_speed)
+    r = "from turtle import *;speed({});onkey(lambda: tracer(0), 'space');listen();title('Press SPACE to skip preview')\n".format(default_speed)
     f = open(output, "w")
     for c in axiom:
         if (c in m):
@@ -85,13 +87,9 @@ def treat_axiom(axiom, m, output):
         else:
             custom_log("error", "Unrecognized character in axiom: " + c)
             exit(1)
-    tracer(1)
-    f.write(r + "exitonclick()")
+    f.write(r + "tracer(1);title('Click anywhere to close');exitonclick()")
     f.close()
     print("Sctipt generated to {} . It can be launched using python {}".format(output, output))
-
-def skip_animation():
-    tracer(0)
 
 movement = {
     "a": a,
@@ -103,10 +101,11 @@ movement = {
     "]": lambda: rewind_to_point(points)
 }
 
-onkey(skip_animation, "space")
+onkey(lambda: tracer(0), "space")
 listen()
 title("Press SPACE to skip preview")
 speed(default_speed)
 treat_axiom(parse_levels(axiom, level, rules), movement, output)
+tracer(1)
 title("Click anywhere to close")
 exitonclick()
