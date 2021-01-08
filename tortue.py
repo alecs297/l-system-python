e842e87d6edaa584ae6d08577164da664c0ef714
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-01-08 20:51:31 +0100

    added rgb and poor attempt at canvas scrolling

--- tortue.py
+++ tortue.py
@@ -13,12 +13,25 @@ default_file = "output.py"
 points = []
 output = default_file
 
-axiom = "a-a-a-a"
+axiom = "a+a+a"
 rules = [
-     "a=a+a*a+a+aa*aa+a+a*a+a"
+     "a=a-a+a"
 ]
-angle = 90
-level = 3
+angle = 120
+level = 7
+
+def update_rgb(rgb):
+    r, g, b = 0, 1, 2
+    if (rgb[r] > 0 and rgb[b] == 0):
+        rgb[r] -= 1
+        rgb[g] += 1
+    elif (rgb[g] > 0 and rgb[r] == 0):
+        rgb[g] -= 1
+        rgb[b] += 1
+    else:
+        rgb[b] -= 1
+        rgb[r] += 1
+    return tuple(rgb)
 
 def custom_log(type, message):
     print("{} [{}] {}".format(strftime("%H:%M:%S", localtime()), type.upper(), message))
@@ -81,13 +94,18 @@ def parse_levels(axiom, levels, rules):
 def treat_axiom(axiom, m, output):
     r = "from turtle import *;speed({});onkey(lambda: tracer(0), 'space');listen();title('Press SPACE to skip preview')\n".format(default_speed)
     f = open(output, "w")
+    rgb = [255, 0, 0]
+    colormode(255)
+    bgcolor("black")
+    screensize(2000, 2000)
     for c in axiom:
         if (c in m):
-            r += (m[c]())
+            pencolor(update_rgb(rgb))
+            f.write(m[c]())
         else:
             custom_log("error", "Unrecognized character in axiom: " + c)
             exit(1)
-    f.write(r + "tracer(1);title('Click anywhere to close');exitonclick()")
+    f.write("tracer(1)")
     f.close()
     print("Sctipt generated to {} . It can be launched using python {}".format(output, output))
 
@@ -107,5 +125,4 @@ title("Press SPACE to skip preview")
 speed(default_speed)
 treat_axiom(parse_levels(axiom, level, rules), movement, output)
 tracer(1)
-title("Click anywhere to close")
-exitonclick()
+done()
